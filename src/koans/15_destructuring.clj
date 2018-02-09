0f2ff73026d004358cf367215aef0c3b33796bfc
Leandro Doctors <ldoctors@gmail.com>
2018-02-09 19:33:10 -0300

    (almost done solving) koan 15 (destructuring)

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -27,17 +27,19 @@
  "You can regain the full argument if you like arguing"
  (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
     (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-      __))
+      (hash-map :original-parts (vector first-name last-name) :named-parts (hash-map :first first-name :last last-name))))
 
  "Break up maps by key"
  (= "123 Test Lane, Testerville, TX"
     (let [{street-address :street-address, city :city, state :state} test-address]
-      __))
+      (apply str (interpose ", " (list street-address city state)))))
 
  "Or more succinctly"
  (= "123 Test Lane, Testerville, TX"
-    (let [{:keys [street-address __ __]} test-address]
-      __))
+    (let [{:keys [street-address city state]} test-address]
+      (clojure.string/join ", " (list (:street-address test-address) (:city test-address) (:state test-address)))
+      (clojure.string/join ", " (vals test-address))))
+
 
  "All together now!"
  (= "Test Testerson, 123 Test Lane, Testerville, TX"
